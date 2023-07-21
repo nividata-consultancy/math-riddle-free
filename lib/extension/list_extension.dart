0a9bb748973808794b2c3aec6eac9645cf1b9b10
mehul425 <m.makwana450@gmail.com>
2023-07-21 18:37:32 +0530

    add assert while create ordered puzzle array

--- lib/extension/list_extension.dart
+++ lib/extension/list_extension.dart
@@ -1,14 +1,17 @@
+import 'package:collection/collection.dart';
 import 'package:flutter/foundation.dart';
 import 'package:tuple/tuple.dart';
 
 extension ListExtension<T> on List<T> {
   List<T> orderByAnotherList(List<int> order, int Function(T) getId) {
-    // assert(order.isNotEmpty, "Game ordering should not be empty.");
-    // assert(this.isNotEmpty, "Game level should not be empty.");
-    // assert(order.length == this.length,
-    //     "Game ordering and level must have same length.");
-    // assert(listEquals(order, this.map((e) => getId(e)).toList()) == true,
-    //     "Some puzzle are missing as order and puzzle list contains different ids.");
+    assert(order.isNotEmpty, "Game ordering should not be empty.");
+    assert(isNotEmpty, "Game level should not be empty.");
+    assert(order.length == length,
+        "Game ordering and level must have same length.");
+    assert(
+        const IterableEquality()
+            .equals(map((e) => getId(e)).toList()..sort(), order..sort()),
+        "Some puzzle are missing as order and puzzle list contains different ids.");
 
     return this
       ..sort((a, b) {
@@ -29,7 +32,7 @@ void main() {
     const Tuple2(4, "D"),
   ];
 
-  var order = [4, 1, 2, 3];
+  var order = <int>[4, 1, 2, 3];
   if (kDebugMode) {
     print(list.orderByAnotherList(order, (p0) => p0.item1).toString());
   }
